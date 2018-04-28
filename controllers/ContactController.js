c3096b29f7015fd4ed3aefe70da147cd053eb9a1
Corey Russ <corey.j.russ@gmail.com>
2018-04-27 23:12:58 -0700

    Checkpoint work completed

---
+++ controllers/ContactController.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const Contact = require('../db/models').Contact;
+
+module.exports = class ContactController {
+
+  constructor() {
+    this.contacts = [];
+    this.addContact.Questions = [
+      {
+        type: 'input',
+        name: 'name',
+        message: "Contact's name - ",
+        validate(val){
+          return val !== '';
+        }
+      }
+    ];
+  }
+
+  addContact(name, phone) {
+    return Contact.create({name, phone})
+  }
+
+}
