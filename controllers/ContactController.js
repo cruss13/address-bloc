8f9e37cca7c005b24c4122c98a1d5aee379ec9f6
Corey Russ <corey.j.russ@gmail.com>
2018-04-28 14:10:10 -0700

    Add contact fixed

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -4,11 +4,27 @@ module.exports = class ContactController {
 
   constructor() {
     this.contacts = [];
-    this.addContact.Questions = [
+    this.addContactQuestions = [
       {
         type: 'input',
         name: 'name',
         message: "Contact's name - ",
+        validate(val){
+          return val !== '';
+        }
+      },
+      {
+        type: 'input',
+        name: 'phone',
+        message: "Contact's phone number - ",
+        validate(val){
+          return val !== '';
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "Contact's email address - ",
         validate(val){
           return val !== '';
         }
